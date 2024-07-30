5d96863659bbe15d00001f81bf54b9ccd6e10db5
wenhwang97 <wenhwang@bu.edu>
2024-07-31 03:04:51 +0800

    add test to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { ChoiceReactionTimeMain } from "./components/ChoiceReactionTimeMain";
+import { DigitSymbolCodingMain } from "./components/DigitSymbolCodingMain";
+import { MemoryRecallMain } from "./components/MemoryRecallMain";
+import { SpacialMemoryMain } from "./components/SpacialMemoryMain";
+import { randomSelectFromList } from "./utils/generalUtils";
+
+const AnimalList = [
+  "Octopus",
+  "Elephant",
+  "Cat",
+  "Lion",
+  "Squirrel",
+  "Kangaroo",
+  "Crocodile",
+  "Pigeon",
+  "Horse",
+  "Fox",
+  "Chicken",
+  "Eagle",
+  "Shark",
+  "Dolphin",
+  "Parrot",
+  "Lizard",
+];
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      {/* <SpacialMemoryMain enabled /> */}
+      {/* <MemoryRecallMain options={AnimalList} selected={["Octopus", "Elephant", "Cat", "Lion", "Squirrel"]} /> */}
+      {/* <ChoiceReactionTimeMain
+        correctIndex={randomSelectFromList([0, 1, 2])}
+        correctSymbol={randomSelectFromList(["<", ">"])}
+      /> */}
+      {/* <DigitSymbolCodingMain correctIndex={Math.floor(Math.random() * 10) + 1} /> */}
     </>
   );
 }
