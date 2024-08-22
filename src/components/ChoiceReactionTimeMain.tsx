4b1b67c431933187f6189ca587af40f9753d3f3a
wenhwang97 <wenhwang@bu.edu>
2024-08-22 23:46:50 +0800

    add random wait timer

--- src/components/ChoiceReactionTimeMain.tsx
+++ src/components/ChoiceReactionTimeMain.tsx
@@ -1,7 +1,8 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { Box, Button, Typography } from "@mui/material";
 import { shuffleList } from "../utils/generalUtils";
 import { choiceReactionTimeConfig as uiConfig } from "../config/uiConfig";
+import { choiceReactionTimeConfig as testConfig } from "../config/testConfig";
 
 interface ChoiceReactionTimeMainProps {
   correctIndex: 0 | 1 | 2;
@@ -14,13 +15,26 @@ export const ChoiceReactionTimeMain: FC<ChoiceReactionTimeMainProps> = ({
   correctSymbol,
   handleSubmit,
 }) => {
+  const [hide, setHide] = useState(true);
+
+  const waitTime =
+    Math.floor(Math.random() * (testConfig.waitTimeMax - testConfig.waitTimeMin)) + testConfig.waitTimeMin;
   const symbols = shuffleList(["<", ">"]);
   const colors = shuffleList([uiConfig.choiceColor.color0, uiConfig.choiceColor.color1]);
 
   const submitHandler = (input: string) => {
     const result = correctSymbol === input;
+    setHide(true);
     handleSubmit(result);
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setHide(false);
+    }, waitTime);
+
+    return () => clearTimeout(timer);
+  }, [handleSubmit]);
 
   return (
     <Box display="flex" alignItems="center" gap={20}>
@@ -43,25 +57,29 @@ export const ChoiceReactionTimeMain: FC<ChoiceReactionTimeMainProps> = ({
       </Button>
       <Box position="relative">
         <Box>
-          {Array.from({ length: 3 }).map((_, index) => (
-            <Box
-              key={index}
-              width={80}
-              height={80}
-              display="flex"
-              alignItems="center"
-              justifyContent="center"
-              border={1}
-              marginY={2}
-              sx={{
-                backgroundColor: index === correctIndex ? colors[0] : colors[1],
-              }}
-            >
-              <Typography variant="h2" fontWeight="bold">
-                {index === correctIndex ? correctSymbol : symbols.pop()}
-              </Typography>
-            </Box>
-          ))}
+          {hide ? (
+            <Box width={82} />
+          ) : (
+            Array.from({ length: 3 }).map((_, index) => (
+              <Box
+                key={index}
+                width={80}
+                height={80}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                border={1}
+                marginY={2}
+                sx={{
+                  backgroundColor: index === correctIndex ? colors[0] : colors[1],
+                }}
+              >
+                <Typography variant="h2" fontWeight="bold">
+                  {index === correctIndex ? correctSymbol : symbols.pop()}
+                </Typography>
+              </Box>
+            ))
+          )}
         </Box>
       </Box>
       <Button
