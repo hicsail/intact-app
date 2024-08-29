a21a4577e619d572c343fedf2c657fa286502fc1
Hazim Ab Halim <hazim.halim54@yahoo.com>
2024-08-29 15:10:29 -0400

    removed unnecessary states

--- src/components/TextToSpeech.tsx
+++ src/components/TextToSpeech.tsx
@@ -1,41 +1,24 @@
-import { FC, ReactElement, useEffect, useState, ReactNode } from "react";
+import { FC, useEffect, useState } from "react";
 import { Box, Button, Typography, TypographyProps } from "@mui/material";
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import { SxProps, SystemProps } from '@mui/system';
-
-
 
 export const TextToSpeech: FC<TypographyProps> = (props) => {
 
     const [audioAvailable, setAudioAvailable] = useState(false);
-    const [iconSize, setIconSize] = useState(16);
-    const [message, setMessage] = useState("");
-
+   
     useEffect(()=> {
         // Checking if browser supports text-to-speech
         // Most browsers supoprt it https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
         if (('speechSynthesis' in window)) {
             setAudioAvailable(true);
         }
-
-        if (props.fontSize) {
-            setIconSize(Number(props.fontSize))
-        }
-
-        if(props.children){
-            setMessage(String(props.children))
-        }
-
     }, [])
 
     const handleClick = () => {
         var msg = new SpeechSynthesisUtterance();
-        msg.text = message;
+        msg.text = String(props.children);
         window.speechSynthesis.speak(msg);
     }
-    
-    
-    
 
     return (
         <Box
@@ -46,7 +29,7 @@ export const TextToSpeech: FC<TypographyProps> = (props) => {
             {audioAvailable &&
                 <Button
                     onClick={handleClick}
-                    endIcon={<PlayArrowIcon style={{fontSize : iconSize}}/>}
+                    endIcon={<PlayArrowIcon style={{fontSize : Number(props.fontSize) || 16}}/>}
                 />
             }
         </Box>
