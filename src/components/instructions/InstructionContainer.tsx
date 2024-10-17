3b948f58ed284e533598961a4af9de7b5ec32d0e
wenhwang97 <wenhwang@bu.edu>
2024-10-17 09:45:08 -0400

    add container for instructions during transition

---
+++ src/components/instructions/InstructionContainer.tsx
@@ -0,0 +1,78 @@
+import { FC, useState } from "react";
+import { TestPhase } from "../../contexts/general.context";
+import { Box, Button, Divider, Typography } from "@mui/material";
+
+const titleMapping: { [key in TestPhase]?: string } = {
+  [TestPhase.MEMORY_RECALL_IMMEDIATE]: "Memory - Immediate Recall",
+  [TestPhase.VISUAL_PAIRS_MEMORIZE]: "Visual Paired Associates - Learn",
+  [TestPhase.CHOICE_REACTION_TIME]: "Choice Reaction Time",
+  [TestPhase.VISUAL_PAIRS_RECALL]: "Visual Paired Associates - Test",
+  [TestPhase.DIGIT_SYMBOL_MATCHING]: "Digit Symbol Matching",
+  [TestPhase.SPATIAL_MEMORY]: "Spatial Memory",
+  [TestPhase.MEMORY_RECALL_DELAYED]: "Memory - Delayed Recall",
+};
+
+export const instructionComponents: { [key in TestPhase]?: JSX.Element[] } = {
+  [TestPhase.MEMORY_RECALL_IMMEDIATE]: [],
+  [TestPhase.VISUAL_PAIRS_MEMORIZE]: [],
+  [TestPhase.CHOICE_REACTION_TIME]: [],
+  [TestPhase.VISUAL_PAIRS_RECALL]: [],
+  [TestPhase.DIGIT_SYMBOL_MATCHING]: [],
+  [TestPhase.SPATIAL_MEMORY]: [],
+  [TestPhase.MEMORY_RECALL_DELAYED]: [],
+};
+
+interface InstructionContainerProps {
+  phase: TestPhase;
+  handleTransition: () => void;
+}
+
+export const InstructionContainer: FC<InstructionContainerProps> = ({ phase, handleTransition }) => {
+  const [instructionIdx, setInstructionIdx] = useState<number>(0);
+
+  return (
+    <>
+      <Box marginX="auto" position="absolute" top="10%" width="85%" sx={{ transform: "translateX(-50%)" }}>
+        <Typography variant="h5" textAlign="initial" fontWeight="bold" marginBottom={1}>
+          {titleMapping[phase]}
+        </Typography>
+        <Divider />
+        {instructionComponents[phase]![instructionIdx]}
+      </Box>
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          display: "flex",
+          justifyContent: "center",
+          p: 1,
+          zIndex: 100,
+        }}
+      >
+        {instructionIdx + 1 < instructionComponents[phase]!.length ? (
+          <Button
+            variant="contained"
+            color="inherit"
+            sx={{ fontSize: 18, padding: 1.5 }}
+            fullWidth
+            onClick={() => setInstructionIdx((idx) => idx + 1)}
+          >
+            Tap here to Continue
+          </Button>
+        ) : (
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ fontSize: 18, padding: 1.5 }}
+            fullWidth
+            onClick={handleTransition}
+          >
+            Start the Test
+          </Button>
+        )}
+      </Box>
+    </>
+  );
+};
