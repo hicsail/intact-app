381c7f7aa95d5d27b526be220818926b66a55f7a
wenhwang97 <wenhwang@bu.edu>
2024-10-17 12:09:55 -0400

    change figures for visual pairs instructions

--- src/components/instructions/VisualPairs.tsx
+++ src/components/instructions/VisualPairs.tsx
@@ -1,17 +1,44 @@
-import { Grid, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import { generalConfig as testConfig } from "../../config/test.config";
 import { FC } from "react";
 
-const VPVisual: FC = () => (
+const VPVisual0: FC = () => (
   <Grid container spacing={1} marginY={2} justifyContent="center">
     <Grid item component="img" height={160} src="/assets/visual-pair/example1.jpg" />
     <Grid item component="img" height={160} src="/assets/visual-pair/example2.jpg" />
   </Grid>
 );
 
+const VPVisual1: FC = () => (
+  <Grid container direction="column" marginY={2}>
+    <Grid container spacing={1} justifyContent="center">
+      <Grid item>
+        <Box component="img" height={84} src={`/assets/visual-pair/example1.jpg`} border="3px solid red" />
+      </Grid>
+      <Grid item>
+        <Box component="img" height={90} src={`/assets/visual-pair/example2.jpg`} />
+      </Grid>
+    </Grid>
+    <Grid container spacing={1} justifyContent="center">
+      {[3, 4].map((option, idx) => (
+        <Grid key={idx} item>
+          <Box component="img" height={90} src={`/assets/visual-pair/example${option}.jpg`} />
+        </Grid>
+      ))}
+    </Grid>
+    <Grid container spacing={1} justifyContent="center">
+      {[5, 6].map((option, idx) => (
+        <Grid key={idx} item>
+          <Box component="img" height={90} src={`/assets/visual-pair/example${option}.jpg`} />
+        </Grid>
+      ))}
+    </Grid>
+  </Grid>
+);
+
 export const vpmInstructions: JSX.Element[] = [
   <>
-    <VPVisual />
+    <VPVisual0 />
     <Typography variant="body1" gutterBottom fontSize={18} textAlign="initial" marginY={2}>
       You will see {Object.keys(testConfig.visualPairsAns).length} image pairs, like above. Learn which images go
       together. Later you will be tested on that.
@@ -21,10 +48,13 @@ export const vpmInstructions: JSX.Element[] = [
 
 export const vprInstructions: JSX.Element[] = [
   <>
-    <VPVisual />
+    <VPVisual0 />
     <Typography variant="body1" gutterBottom fontSize={18} textAlign="initial" marginTop={2}>
       Let's test your memory for the images you learned a few minutes ago.
     </Typography>
+  </>,
+  <>
+    <VPVisual1 />
     <Typography variant="body1" gutterBottom fontSize={18} textAlign="initial" marginBottom={2}>
       You will need to select the image that goes together with the image with <strong>red outline</strong> at the{" "}
       <strong>top left</strong> corner.
@@ -32,7 +62,7 @@ export const vprInstructions: JSX.Element[] = [
   </>,
   <>
     <Typography variant="body1" fontSize={18} textAlign="initial" marginY={2}>
-      You will be asked to recall all {Object.keys(testConfig.visualPairsAns).length} pairs. Let's start!
+      You will be asked to recall all {Object.keys(testConfig.visualPairsAns).length} pairs of images. Let's start!
     </Typography>
   </>,
 ];
